a428c7dd6229134f232df83844c08c0d4d757d8d
syharash <138817364+syharash@users.noreply.github.com>
2025-07-29 10:08:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = "515935803707-v7qshp425m1b4h5ru6jc0HrEZbgAhkF7zRGU1Nw.apps.googleusercontent.com";
+const CLIENT_ID = "515935803707-v7qshp425m1b4h5ru6jcmmmu99qbikgq.apps.googleusercontent.com";
 const DEVELOPER_KEY = "AIzaSyCl6PFx1jCh7xjc0HrEZbgAhkF7zRGU1Nw";
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
 
