bb7150022673e84900bc81c9260a57b244b6516f
syharash <138817364+syharash@users.noreply.github.com>
2025-07-30 10:07:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,194 +1,184 @@
 const CLIENT_ID = "515935803707-v7qshp425m1b4h5ru6jcmmmu99qbikgq.apps.googleusercontent.com";
+const API_KEY = "AIzaSyCl6PFx1jCh7xjc0HrEZbgAhkF7zRGU1Nw"; // Replace with actual API key
+
 // const DEVELOPER_KEY = "AIzaSyCl6PFx1jCh7xjc0HrEZbgAhkF7zRGU1Nw";
 // const SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-let access
+let accessToken = "";
+
 const sheetIndex = {
   "California/Elk Groove/USA": {
     id: "1-YP4QqW4wmfqwgRMXmbEtURiyw2pi3nkYLGbc0sQlT4",
-    range: "Sheet1!A8:AM10"
+    range: "Sheet1!A4:D41"
   },
   "California/Riverside/USA": {
     id: "1iygvJ-hGlaBT23zKlVJksdUcU3xOMtZ10wFvgUa5MnA",
-    range: "Riverside!A8:AM10"
+    range: "Riverside!A4:D41"
   },
   "California/Sacramento/USA": {
     id: "148TnNDI74ZQg5CX4lukFaK_lbVjnG-TaVVJEdN3s_ok",
-    range: "Sacramento!A7:AM10"
+    range: "Sacramento!A4:D41"
   },
   "California/Woodland/USA": {
     id: "1Dr4mbBqPkTvKR6OBfpCiHvz7CJsUxlzw-rtu-cXjRlA",
-    range: "Woodland!A7:AM10"
+    range: "Woodland!A4:D41"
   },
   "California/Yuba City/USA": {
     id: "1264Z5C0il28XgXsXf_49wZ4QhcDRD2LGUeb0aOhL7mk",
-    range: "YubaCity!A7:AM10"
+    range: "YubaCity!A4:D41"
   },
   "Florida/Miami/USA": {
     id: "1PXMZO1LGWo4TQU2k3D45unvQN7fhZ9GDT6d1ox6tI8Y",
-    range: "Miami!A7:AM10"
+    range: "Miami!A4:D41"
   },
   "Georgia/Lilburn/USA": {
     id: "10JI4HWahbH3H9b4WjjA6oMCt6fjpIdcsXLpo58ZuCz4",
-    range: "Lilburn!A7:AM10"
+    range: "Lilburn!A4:D41"
   },
   "Illinois/Bloomington/USA": {
     id: "173tvkXPDyX1wUa6W7hPRbXxhwKxzmLSYqAFBmq4ZvnI",
-    range: "Bloomington!A7:AM10"
+    range: "Bloomington!A4:D41"
   },
   "Illinois/Chicago/USA": {
     id: "1vOI4y7A0SfbkrcFPjyQ5cgydGTGrycrwglbs0yvxIRw",
-    range: "Chicago!A7:AM10"
+    range: "Chicago!A4:D41"
   },
   "Illinois/Lombard/USA": {
     id: "1MQN_oKrmy92tTYpXoTbJG78tNKM-ENzCCeMDs4__Gro",
-    range: "Lombard!A7:AM10"
+    range: "Lombard!A4:D41"
   },
   "Illinois/Schaumburg/USA": {
     id: "1NP35gD_FGznqtI1-SzLvJ4mkwCcagNGvQPCvAdLI_wM",
-    range: "Schaumburg!A7:AM10"
+    range: "Schaumburg!A4:D41"
   },
   "Illinois/Skokie/USA" : {
     id: "1piIR4er7HwXN00iFNzOkbGrgaQAVb_rxIGy8za89UhY",
-    range: "Skokie!A7:AM10"
+    range: "Skokie!A4:D41"
   },
   "Maryland/Baltimore/USA" : {
     id: "1zAh1Vbg82heZCviFknrdKSoC-PMjptrE5JBWEm-T5oE",
-    range: "Baltimore!A7:AM10"
+    range: "Baltimore!A4:D41"
   },
   "Maryland/Ellicot City/USA" : {
     id: "1LwhIQaM50PrJfkr4r6U964iwC1pJnq1dP_k0cAEPqKw",
-    range: "EllicotCity!A7:AM10"
+    range: "EllicotCity!A4:D41"
   },
   "Maryland/Langhorne/USA" : {
     id: "1zDniVeYL_dTZpvGS18pmklmmVDaCF0WGd3gX4yM9Y8A",
-    range: "Langhorne!A7:AM10"
+    range: "Langhorne!A4:D41"
   },
   "Maryland/Woodbridge/USA" : {
     id: "1pOkArNYjQWyD-qRWG_-VxDRTH9AAoxX_GcnlPfnTJB8",
-    range: "Woodbridge!A7:AM10"
+    range: "Woodbridge!A4:D41"
   },
   "New York/Bronx/USA" : {
     id: "19flAXbGnWQBTV9-GWTNokrQvEpgAJ83g2eoIKI-Fpek",
-    range: "Bronx!A7:AM10"
+    range: "Bronx!A4:D41"
   },
   "New York/Brooklyn/USA" : {
     id: "1olu4ln0OKYtsbWPIjtGerpbkUZqbxnePfUOjJ0D6eG4",
-    range: "Brooklyn!A7:AM10"
+    range: "Brooklyn!A4:D41"
   },
   "New York/Queens/USA" : {
     id: "1I6yvtg-XLON4TDbLRufS92_R58BAdpErP_KB3Wn7uEE",
-    range: "Queens!A7:AM10"
+    range: "Queens!A4:D41"
   },
   "New York/Valley Stream/USA" : {
     id: "1nTAnMynDiNJ05HhbfWieqz4a7nVgh6iRYD33n02R2og",
-    range: "ValleyStream!A7:AM10"
+    range: "ValleyStream!A4:D41"
   },
   "Texas/New Orleans/USA" : {
     id: "1XMJtXTPCGDrtHmoz8xgx_qoeSfylDUDP8l1iTZs9PlI",
-    range: "NewOrleans!A7:AM10"
+    range: "NewOrleans!A4:D41"
   },
   "Texas/Sugar Land/USA" : {
     id: "1YGC_xaiFWDI31SJhF5oa6gmFTOzktgGtKLpOoh02xIs",
-    range: "SugarLand!A7:AM10"
+    range: "SugarLand!A4:D41"
   },
   "Texas/Wylie/USA" : {
     id: "1JBhROkoHG09HzvaQxCO0WUjtUgHO8m7tySSZlxJFVCE",
-    range: "Wylie!A7:AM10"
+    range: "Wylie!A4:D41"
   },
   "Washington/Lynwood/USA" : {
     id: "1PAXcYLnUw-zKTuExtg6vAA84tPI29bZrNVdh_9UtOSw",
-    range: "Lynnwood!A7:AM10"
+    range: "Lynnwood!A4:D41"
   },
   "California/USA" : {
     id: "14LBe2hZvwx4lBik9k1jkPJvIrKoV_7X-RcYazHrnyrg",
-    range: "California!A7:AM10"
+    range: "California!A4:D41"
   },
   "Florida/USA" : {
     id: "1YAljj_sVKMp4fr7ShpFfy4EtSK1Dk_vqOXyS24KQ4kM",
-    range: "Florid!A7:AM10"
+    range: "Florid!A4:D41"
   },
   "Georgia/USA" : {
     id: "1asCNMkGvJcPqe6uSoPmdyNmgnfroMtQIvf7wzXDLQ6Q",
-    range: "Georgia!A7:AM10"
+    range: "Georgia!A4:D41"
   },
   "Illinois/USA" : {
     id: "12HPghBjP6v8LCvFIJ_Pe1TXAnQdaEgfjaJRBFR27cEQ",
-    range: "Illinois!A7:AM10"
+    range: "Illinois!A4:D41"
   },
   "Maryland/USA" : {
     id: "1ssgqClME3RbZxGqNfdQeT7bbZSwVk-fx-xWGJcLLh78",
-    range: "Maryland!A7:AM10"
+    range: "Maryland!A4:D41"
   },
   "New York/USA" : {
     id: "1znKOsK3qW8sfJGsnkuKGiiuMJ9rvRRTxr4RWjw9kZAw",
-    range: "NewYork!A7:AM10"
+    range: "NewYork!A4:D41"
   },
   "Texas/USA" : {
     id: "1wC4XLsWbAKiciLpIOKU6WeurEjEQpngXdgBI051_Iec",
-    range: "Texas!A7:AM10"
+    range: "Texas!A4:D41"
   },
   "Washington/USA" : {
     id: "1NQSfiwuRitYZkeBM7f6iF-ivrzpxAZpTo65Vh2Y50BM",
-    range: "Washington!A7:AM10"
+    range: "Washington!A4:D41"
   },
   "USA" : {
     id: "15sKA8JESDMKYD0t0Qc0INMOSKds7oKJ9Funok4TvFbs",
-    range: "USA!A7:AM10"
+    range: "USA!A4:D41"
   }
 };
 
-
 const metricLabels = [
-  "Weekly Booklet Reading",
-  "Pious Deeds Booklet",
-  "Waking up for fajr",
-  "Tafseer Session",
-  "Madani Qafila",
-  "Dars",
-  "Madrasah Balighan",
-  "Madani Courses",
-  "Individual Efforts",
-  "Weekly Ijtima",
-  "Madani Muzakara",
-  "Area Visit",
-  "A Day / Other Languages",
-  "Readers & Listener",
-  "Distributed",
-  "Total Participants",
-  "Received",
-  "Sub Unit Places",
-  "3 Days Quantity",
-  "In Masjid",
-  "3 Days Travellers",
+  "Waking up for Fajr",
+  "Tafseer",
+  "Dars in Masjid",
   "Home Dars",
-  "12 Days Quantity",
-  "Chowk / Area Dars",
-  "12 Days Travellers",
+  "Area Dars",
   "Total Dars",
-  "1 Month Quantity",
-  "In Masjid",
-  "1 Month Travellers",
-  "Total Participants",
-  "Total Qafila",
-  "At Other Places",
-  "Total Participants Qafila",
-  "Total Participants",
-  "Total Madani Courses",
+  "Madarsa in Home",
+  "Participants in Home",
+  "Madarsa at other places",
+  "Participants at other places",
   "Total Madaris",
-  "Total Course Participants",
-  "Daily Participants",
-  "Monthly Participants",
-  "Total Ijtema",
-  "Ijtema Participants",
-  "Night Etikaf",
-  "Etikaf Sub Units",
-  "Etikaf Participants",
-  "Etikaf Times",
-  "Etikaf Total Participants",
-  "Times (A Day)",
-  "Times (Other Lang)",
-  "Participants (Other Lang)"
-];
+  "Total Students",
+  "Daily Individual Efforts",
+  "Monthly Individual Efforts",
+  "Ijtema Qty",
+  "Participants in Ijtema",
+  "Participants in Night Etikaf",
+  "Madani Muzakaras",
+  "Madani Muzakara Participants",
+  "How Many Times Area Visit",
+  "Total Participants in Area Visit",
+  "How-many-Times (A Day in the Path of the Almighty)",
+  "How-many-Times (Session in other Languages)",
+  "Total Participants (Session in other Languages)",
+  "Numbers of Readers & Listener of Weekly Booklet",
+  "Pious Booklet Distributed",
+  "Pious Booklet Received",
+  "Qafilah 3 Days Quantity",
+  "Qafilah 3 Days Travellers",
+  "Qafilah 12 Days Quantity",
+  "Qafilah 12 Days Travellers",
+  "Qafilah 1 Month Quantity",
+  "Qafilah 1 Month Travellers",
+  "Total Number Qafilas",
+  "Total Participants Qafilas",
+  "Total Number Madani Courses",
+  "Total Number of Participants",
+ ];
 
 const getAuthToken = () => {
   // Your OAuth token logic here — ideally loaded from localStorage or a secure flow
